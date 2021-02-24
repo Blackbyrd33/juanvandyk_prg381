26a1e919fa2efe42ed9cfa285bbdc9203307478d
Blackbyrd33 <vdykj1@gmail.com>
2021-02-24 11:06:44 +0200

    Started changes on develop

--- prg381.java
+++ prg381.java
@@ -3,9 +3,9 @@ public class prg381 {
     public static void main(String[] args) {
         System.out.println("First File on Git");
         System.out.println("First File on Git");
-        System.out.println("Test");
+
         //comment
-        //comment2
-        //comment3
+        System.out.println("Test");
+       
     }
 }
